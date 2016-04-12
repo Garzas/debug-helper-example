b063c2ae335ac8fb8be0158c28dfc8b34a44db5c
Kuba Żerko <zerasop@gmail.com>
2016-04-12 16:49:21 +0200

    added presenter for MainActivity

--- app/src/main/java/com/appunite/example/debugutilsexample/main/MainActivity.java
+++ app/src/main/java/com/appunite/example/debugutilsexample/main/MainActivity.java
@@ -1,17 +1,31 @@
 package com.appunite.example.debugutilsexample.main;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.appunite.example.debugutilsexample.App;
 import com.appunite.example.debugutilsexample.BaseActivity;
 import com.appunite.example.debugutilsexample.R;
 import com.appunite.example.debugutilsexample.dagger.ActivityModule;
 import com.appunite.example.debugutilsexample.dagger.BaseActivityComponent;
+import com.appunite.example.debugutilsexample.presenter.MainPresenter;
+import com.jakewharton.rxbinding.widget.RxTextView;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class MainActivity extends BaseActivity {
+
+    @Inject
+    MainPresenter presenter;
+
+    @InjectView(R.id.simple_text)
+    TextView simpleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,11 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         MainActivityComponent component = (MainActivityComponent) getActivityComponent();
         component.inject(this);
+        ButterKnife.inject(this);
+
+        presenter.getStringObservable()
+                .compose(this.<String>bindToLifecycle())
+                .subscribe(RxTextView.text(simpleText));
     }
 
     @Nonnull
--- app/src/main/java/com/appunite/example/debugutilsexample/presenter/MainPresenter.java
+++ app/src/main/java/com/appunite/example/debugutilsexample/presenter/MainPresenter.java
@@ -1,4 +1,23 @@
 package com.appunite.example.debugutilsexample.presenter;
 
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+
+import rx.Observable;
+
 public class MainPresenter {
+
+    @Nonnull
+    private final Observable<String> stringObservable;
+
+    @Inject
+    public MainPresenter() {
+        stringObservable = Observable.just("text from presenter");
+    }
+
+    @Nonnull
+    public Observable<String> getStringObservable() {
+        return stringObservable;
+    }
 }
