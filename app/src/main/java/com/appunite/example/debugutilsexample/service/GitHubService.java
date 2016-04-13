e806d87bbd1ebd16e7a894854e7f4c1bb14936e7
Kuba Żerko <zerasop@gmail.com>
2016-04-13 14:43:13 +0200

    added header

--- app/src/main/java/com/appunite/example/debugutilsexample/service/GitHubService.java
+++ app/src/main/java/com/appunite/example/debugutilsexample/service/GitHubService.java
@@ -5,11 +5,13 @@ import com.appunite.example.debugutilsexample.model.Repos;
 import java.util.List;
 
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import rx.Observable;
 
 public interface GitHubService {
 
+    @Headers("Accept: application/vnd.github.v3+json")
     @GET("/orgs/{name}/repos")
     Observable<List<Repos>> getRespos(
             @Path("name") String name
