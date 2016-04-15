02ed8bfe440e7479419dcf62a76d009fdf6de7d6
Kuba Żerko <zerasop@gmail.com>
2016-04-15 12:15:50 +0200

    added tests for MainPresenter

--- app/src/test/java/com/appunite/example/debugutilsexample/presenter/MainPresenterTest.java
+++ app/src/test/java/com/appunite/example/debugutilsexample/presenter/MainPresenterTest.java
@@ -1,4 +1,80 @@
 package com.appunite.example.debugutilsexample.presenter;
 
+import com.appunite.example.debugutilsexample.dao.GitHubDao;
+import com.appunite.example.debugutilsexample.model.Repos;
+import com.appunite.rx.ResponseOrError;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.observers.TestObserver;
+
+import static com.google.common.truth.Truth.assert_;
+import static org.mockito.Mockito.when;
+
 public class MainPresenterTest {
+
+    @Mock
+    GitHubDao gitHubDao;
+
+    private MainPresenter presenter;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        when(gitHubDao.getReposObservable()).thenReturn(
+                Observable.just(ImmutableList.<Repos>builder()
+                        .add(new Repos(1, "name", "des1", true))
+                        .add(new Repos(2, "name2", "des2", false))
+                        .build())
+                        .compose(ResponseOrError.<List<Repos>>toResponseOrErrorObservable()));
+        presenter = new MainPresenter(gitHubDao);
+
+    }
+
+    @Test
+    public void checkIfRepositoriesAreCorrect() {
+        TestObserver<List<MainPresenter.BaseItem>> listTestObserver = new TestObserver<>();
+
+        presenter.getItemListObservable().subscribe(listTestObserver);
+
+        assert_().that(listTestObserver.getOnNextEvents()).isNotEmpty();
+        assert_().that(getLastOnNextEvent(listTestObserver).size()).isEqualTo(2);
+    }
+
+    @Test
+    public void checkIfRepoItemIsCorrectlyCreated() {
+        TestObserver<List<MainPresenter.BaseItem>> listTestObserver = new TestObserver<>();
+
+        presenter.getItemListObservable().subscribe(listTestObserver);
+
+        MainPresenter.RepoItem repoItem = (MainPresenter.RepoItem) getLastOnNextEvent(listTestObserver).get(0);
+
+        assert_().that(repoItem.getName()).isEqualTo("name");
+    }
+
+    @Test
+    public void checkIfTitleHasValue() {
+        TestObserver<String> testObserver = new TestObserver<>();
+
+        presenter.getTitleObservable().subscribe(testObserver);
+
+        assert_().that(getLastOnNextEvent(testObserver)).isNotEmpty();
+    }
+
+    public static <T> T getLastOnNextEvent(TestObserver<T> testObserver) {
+        return Iterables.getLast(testObserver.getOnNextEvents());
+    }
+
+    public static <T> T getFirstOnNextEvent(TestObserver<T> observer) {
+        return Iterables.getFirst(observer.getOnNextEvents(), null);
+    }
 }
